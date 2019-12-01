179f20140324630cdb8926a300b693201b03c48b
Brandon <brandon@qwang.org>
2019-12-01 07:29:12 +0100

    just commit changes from last time

--- src/main.rs
+++ src/main.rs
@@ -50,18 +50,3 @@ fn read_file() -> Result<(), Box<dyn std::error::Error>> {
 
 }
 
-#[cfg(test)]
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    // use super::*;
-    
-    #[test]
-    fn main() {
-        assert_ne!(4, 3);
-    }
-    
-    #[test]
-    fn read_file(){
-        assert_eq!(4, 4);
-    }
-}
